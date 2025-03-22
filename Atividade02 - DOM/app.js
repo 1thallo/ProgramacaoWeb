74e53b37408208156398c0d83c898c2d52952532
1thallo <ithalloflu@gmail.com>
2025-03-22 08:16:08 -0300

    add Funções - contarCaracteres, adicionarItem e limparTudo

--- Atividade02 - DOM/app.js
+++ Atividade02 - DOM/app.js
@@ -8,6 +8,7 @@ function iteracao(valorIteracao){
     if (valorIteracao > 0) {
         cliques++;
         document.getElementById('countClique').innerText = `${cliques} cliques`;
+        document.getElementById('countClique').style.color = "green";
     }
 
     if (contador < 0) {
@@ -35,4 +36,47 @@ function textoDinamico(event){
 
 function limpar(){
     document.getElementById("textos").innerHTML = "";
+}
+
+function contarCaracteres(){
+    let texto = document.getElementById("caracteres_input").value;
+    let caracteres = texto.replace(/\s/g, "").length; // remove espacos antes de contar
+    document.getElementById("countCaracteres").innerText = `${caracteres} caracteres`;
+}
+
+function adicionarItem() {
+    let tipoLista = document.getElementById("tipo-lista").value;
+    let textoItem = document.getElementById("item-lista").value.trim();
+    if (!textoItem) return;
+
+    let listasDiv = document.getElementById("listas");
+    let lista = listasDiv.querySelector(tipoLista) || listasDiv.appendChild(document.createElement(tipoLista));
+
+    let item = document.createElement("li");
+    item.innerText = textoItem;
+    lista.appendChild(item);
+    document.getElementById("item-lista").value = "";
+}
+
+function limparTudo() {
+    // zerar o contador e cliques
+    contador = 0;
+    cliques = 0;
+    document.getElementById("countNum").innerText = contador;
+    document.getElementById("countClique").innerText = `${cliques} cliques`;
+    document.getElementById("countClique").style.color = "";
+
+    // limpa as listas
+    document.getElementById("listas").innerHTML = "";
+
+    // limpa paragrafos
+    document.getElementById("textos").innerHTML = "";
+
+    // limpa os inputs
+    document.getElementById("texto_input").value = "";
+    document.getElementById("caracteres_input").value = "";
+    document.getElementById("item-lista").value = "";
+
+    // limpa contador de caracteres
+    document.getElementById("countCaracteres").innerText = "0 caracteres";
 }
